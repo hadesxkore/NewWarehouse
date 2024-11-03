9c6908ebab4db5012973baa3f2bfc6c4d8756ee1
Kobie <kobievillanueva26@gmail.com>
2024-11-03 19:25:22 +0800

    updated (5)

--- src/components/Conversation.js
+++ src/components/Conversation.js
@@ -40,7 +40,7 @@ const Conversation = () => {
                 // Determine the role based on rentedWarehouses collection
                 const ownerParticipantId = conversationData.participants.find(participant => participant !== currentUserId);
                 const rentedWarehousesRef = firestore.collection('rentedWarehouses');
-                const rentedWarehousesSnapshot = await rentedWarehousesRef.where('userUid', '==', ownerParticipantId).get();
+                const rentedWarehousesSnapshot = await rentedWarehousesRef.where('ownerUid', '==', ownerParticipantId).get();
                 
                 if (!rentedWarehousesSnapshot.empty) {
                     setOwnerRole('Lessor');
